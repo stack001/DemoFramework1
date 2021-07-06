f8787f6b5f6e8777aa6e5bcd11148dbbd7238287
st <mala64ka@gmail.com>
2021-07-06 17:39:52 +0530

    first Stagging and commiting to reposiratory

---
+++ MyFierstproject/src/MyFierstproject/firstDemo.java
@@ -0,0 +1,16 @@
+package MyFierstproject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class firstDemo {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.gecko.driver","D:\\eclipse-workspace\\selenium\\browser\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		
+		
+		
+	}
+
+}
